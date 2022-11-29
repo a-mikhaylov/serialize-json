55559c593a124cd02cfb80169b1ed3e5b9029917
a-mikhaylov <lexarobotr2d2@gmail.com>
2022-11-29 15:15:13 +0300

    merging jsons

--- header.hpp
+++ header.hpp
@@ -5,30 +5,70 @@
 #include <chrono>
 #include "nlohmann/json.hpp"
 
-#define FIRST_FILE   "num1.json"
-#define SECOND_FILE   "num2.json"
+#define A_FILE   "A_struct.json"
+#define B_FILE   "B_struct.json"
 
 using json = nlohmann::json;
 using namespace std;
 using namespace std::chrono;
 
+json merge(const json &a, const json &b)
+{
+    json result = a.flatten();
+    json tmp = b.flatten();
+
+    for (json::iterator it = tmp.begin(); it != tmp.end(); ++it)
+    {
+        result[it.key()] = it.value();
+    }
+
+    return result.unflatten();
+}
 
 class A {
 public:
 	int32_t k;
+    int a[10];
+    
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(A, k, a);
+
 	A(int window) {
 		this->k = window;
 	}
     A() {
-		this->k             = 987;
+		this->k = 987;
 	}
-    void printSMF() {
-        std::cerr << endl;
-        std::cerr << "k:\t\t" <<this->k << std::endl;
-        std::cerr << "\n\n";
+
+    void printA() {
+        std::cerr << "k:\t" << k << std::endl;
+        std::cerr << "a:";
+
+        for (int i = 0; i < 10; i++) {
+            std::cerr << '\t' << a[i] << std::endl;
+        }
+        cerr << endl;
     }
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(A, k);
+    void Save() {
+        std::ofstream out(A_FILE);
+        json j;
+
+        to_json(j, *this);
+
+        out << j.dump(4) << std::endl;
+        out.close();
+    }
+
+    void Load() {
+        std::ifstream inp(A_FILE);
+        if (!inp.is_open()) return;
+        json ji;
+
+        inp >> ji;
+        from_json(ji, *this);
+        return;    
+    }
+
 };
     
 class B {
@@ -37,20 +77,44 @@ public:
     int y;
     A* ptr;
 
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(B, x, y); 
+    //NLOHMANN_DEFINE_TYPE_INTRUSIVE(B, x, y, ptr); - error
+
     B() {
         this->x = 0;
         this->y = 1;
-    }
-    void printDIF() {
-        cerr << "mama:\t" << x << endl;
-        cerr << "papa:\t" << y << endl;
-        cerr << "\n\n";
+        ptr = nullptr;
     }
 
     B(A* base) : x(base->k), y(base->k), ptr(base) {}
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(B, x, y); 
-    //NLOHMANN_DEFINE_TYPE_INTRUSIVE(B, x, y, ptr); - error
+    void printB() {
+        cerr << "x:\t" << x << endl
+             << "y:\t" << y << endl
+             << "*ptr:\t";
+        if (ptr != nullptr) ptr->printA();
+        cerr << endl;
+    }
+
+    void Save() {
+        std::ofstream out(B_FILE);
+        json j;
+
+        to_json(j, *this);
+
+        out << j.dump(4) << std::endl;
+        out.close();
+    }
+
+    void Load() {
+        std::ifstream inp(B_FILE);
+        if (!inp.is_open()) return;
+        json ji;
+
+        inp >> ji;
+        from_json(ji, *this);
+        return;    
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -2,19 +2,25 @@
 
 int main(int argc, char* argv[]) {
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-
-    A aaa(25);
-    serealize_json(&aaa, FIRST_FILE);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
-    A aParse;
-    deserealize_json(&aParse, FIRST_FILE);
+    A aaa(25); aaa.printA(); aaa.Save();
+    
+    A aParse; aParse.Load(); aParse.printA();
 
-    B bbb{&aParse};
-    serealize_json(&bbb, SECOND_FILE);
+    B bbb{&aParse}; bbb.printB(); bbb.Save();
 
-    B bParse;
-    deserealize_json(&bParse, SECOND_FILE);
+    B bParse; bParse.Load(); bParse.printB();
 
+
+    json jA; to_json(jA, aaa);
+    json jB; to_json(jB, bbb);
+
+    jB = merge(jA, jB);
+    ofstream out("test_insert.json");
+    out << jB.dump(4) << std::endl;
+    out.close();
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>( t2 - t1 ).count();
     cout << endl << endl;
